9d523d2a8938a6d64811a5f251379559744cd410
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-02-14 15:27:57 -0500

    Update FLASK_WebApp.py

--- Back_End/WebApp Simulation/FLASK_WebApp.py
+++ Back_End/WebApp Simulation/FLASK_WebApp.py
@@ -15,8 +15,9 @@
 # Integrated New IMPACT_PUPIL_v1.3.py for script call.
 
 from flask import *
-import os
 import pandas as pd
+import boto3
+import os
 
 option_val = ""
 
@@ -42,11 +43,6 @@ def PupilPain():
     return render_template('PupilPain_Form.html')
 
 
-@app.route('/FacePupilPain')
-def FacePupilPain():
-    return render_template('Pupil_from_Face.html')
-
-
 @app.route('/Upload')
 def Upload():
     return render_template('FileUpload.html')
@@ -68,7 +64,7 @@ def UploadPupil():
             print("Eye color selected: " + eye_color_val)
             fname = eye_color_val + fname_txtfield + lname_txtfield + '.mp4'
             write_to_txt(fname_txtfield, lname_txtfield, eye_color_val, fname, 1, video_type1)
-            PUPIL_UPLOAD_FOLDER = '/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Pupil_Input_Videos/'
+            PUPIL_UPLOAD_FOLDER = './static/Pupil_Input_Videos/'
             app.config['PUPIL_UPLOAD_FOLDER'] = PUPIL_UPLOAD_FOLDER
             f.save(os.path.join(app.config['PUPIL_UPLOAD_FOLDER'], fname))
         print("File Uploaded: " + f.filename)
@@ -91,39 +87,16 @@ def UploadFacial():
             print("Option Selected: " + option_val)
             face_fname = fname_txtfield + lname_txtfield + '.avi'
             write_to_txt(fname_txtfield, lname_txtfield, option_val, face_fname, 2, video_type2)
-            FACIAL_UPLOAD_FOLDER = '/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Face_Input_Videos/'
+            FACIAL_UPLOAD_FOLDER = './static/Face_Input_Videos/'
             app.config['FACIAL_UPLOAD_FOLDER'] = FACIAL_UPLOAD_FOLDER
             f.save(os.path.join(app.config['FACIAL_UPLOAD_FOLDER'], face_fname))
         print("File Uploaded: " + f.filename)
         return render_template('Calculate_Facial.html')
 
 
-@app.route('/UploadNIRColorFace', methods=['GET', 'POST'])
-def UploadNIRColorFace():
-    global Opt
-    global video_type3
-    if request.method == 'POST':
-        fname_txtfield = request.form['firstname']
-        lname_txtfield = request.form['lastname']
-        option_val = request.form['radio']
-        video_type = request.form['radio1']
-        f = request.files['file']
-        global flname
-        if "Video" in option_val:
-            Opt = 2
-            print("Option Selected: " + option_val)
-            flname = option_val + fname_txtfield + lname_txtfield + '.avi'
-            write_to_txt(fname_txtfield, lname_txtfield, option_val, flname, 3, video_type3)
-            FACE_UPLOAD_FOLDER = '/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Face_NIR_Color_Videos/'
-            app.config['FACE_UPLOAD_FOLDER'] = FACE_UPLOAD_FOLDER
-            f.save(os.path.join(app.config['FACE_UPLOAD_FOLDER'], flname))
-        print("File Uploaded: " + f.filename)
-        return render_template('Calculate_Pupil_From_Face.html')
-
-
 @app.route('/Process_Pupil')
 def Process_Pupil():
-    os.system('cd ~/PycharmProjects/FaceEmotionRecognition/')
+    # os.system('cd ./static')
     os.system('python IMPACT_PUPIL_v1.3.py '+str(opt)+' '+str(fname)+' '+str(video_type1))
     res_img_fold = os.path.join('static', 'Pupil_Output_Images')
     res_vid_fold = os.path.join('static', 'Pupil_Output_Videos')
@@ -145,7 +118,7 @@ def Process_Pupil():
 
 @app.route('/Process_Facial')
 def Process_Facial():
-    os.system('cd ~/PycharmProjects/FaceEmotionRecognition/')
+    # os.system('cd /AWS_Lambda/')
     os.system('python IMPACT_FACIAL_v1.0.py '+str(option)+' '+str(face_fname)+' '+str(video_type2))
     res_img_fold = os.path.join('static', 'Facial_Output_Images')
     app.config['FACIAL_OUTPUT_FOLDER'] = res_img_fold
@@ -162,27 +135,12 @@ def Process_Facial():
     return render_template('Facial_Success.html', image_file=pic, max_pain=max_pain_score, mean_pain=mean_pain_score, min_pain=min_pain_score)
 
 
-@app.route('/Process_Pupil_From_Face')
-def Process_Pupil_From_Face():
-    os.system('cd ~/PycharmProjects/FaceEmotionRecognition/')
-    os.system('python Face_Eyes_Iris.py '+str(Opt)+' '+str(flname)+' '+str(video_type3))
-    res_img_fold = os.path.join('static', 'Face_Pupil_Output_Images')
-    app.config['OUTPUT_FOLDER'] = res_img_fold
-    img_name = str(os.path.splitext(flname)[0])
-    file = img_name + '_Avg_Dilation_Plot.png'
-    pic = os.path.join(app.config['OUTPUT_FOLDER'], file)
-    return render_template('Pupil_Success.html', image_file=pic)
-
-
 def write_to_txt(fnametxt, lnametxt, v, fln, flag, vid_type):
     if flag == 1:
-        F = open('/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Pupil_Output_Images/' + v + fnametxt + lnametxt + ".txt", "a+")
+        F = open('./static/Pupil_Output_Images/' + v + fnametxt + lnametxt + ".txt", "a+")
         F.write("Patient Name : " + fnametxt + " " + lnametxt + "\nEye Color : " + v + "\nVideo File-Name : " + fln + "\nVideo Type : " + vid_type +"\n\n")
     elif flag == 2:
-        F = open('/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Facial_Output_Images/' + fnametxt + "_" + lnametxt + ".txt", "a+")
-        F.write("Patient Name : " + fnametxt + " " + lnametxt + "\nVideo Type : " + v + "\nVideo File-Name : " + fln + "\nVideo Code : " + vid_type +"\n\n")
-    else:
-        F = open('/Users/pranavdeo/PycharmProjects/FaceEmotionRecognition/static/Face_Pupil_Output_Images/' + fnametxt + "_" + lnametxt + ".txt", "a+")
+        F = open('./static/Facial_Output_Images/' + fnametxt + "_" + lnametxt + ".txt", "a+")
         F.write("Patient Name : " + fnametxt + " " + lnametxt + "\nVideo Type : " + v + "\nVideo File-Name : " + fln + "\nVideo Code : " + vid_type +"\n\n")
 
     F.close()
@@ -190,4 +148,4 @@ def write_to_txt(fnametxt, lnametxt, v, fln, flag, vid_type):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
