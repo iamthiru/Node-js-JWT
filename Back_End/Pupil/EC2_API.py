2245a03894393a4dbbfa4872bcec2fceb30750ad
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-02-17 11:36:29 -0500

    Update EC2_API.py

--- Back_End/Pupil/EC2_API.py
+++ Back_End/Pupil/EC2_API.py
@@ -31,18 +31,14 @@ try:
     client.connect(hostname=instance_ip, username="ubuntu", pkey=key)
 
     # Send file from local to EC2 instance.
-    print('1')
     os.system('scp -i ~/Downloads/imp_benten.pem ' + Video_File + ' ubuntu@3.87.214.251:Video_Data/')
-    print('2')
     stdin1, stdout1, stderr1 = client.exec_command('sudo docker cp ~/Video_Data/' + Video_name + ' a2bb32b4eae6:/AWS_Lambda/static/Pupil_Input_Videos/' + Video_name)
 
     # API Call to run the back-end algorithm.
-    print('3')
     stdin2, stdout2, stderr2 = client.exec_command('sudo docker exec a2bb32b4eae6 bash -c "python IMPACT_PUPIL_v1.3.py 1 ' + Video_name + ' Color"', timeout=60)
     time.sleep(5)
 
     # Code to download the output files:
-    print('4')
     stdin3, stdout3, stderr3 = client.exec_command('sudo docker cp a2bb32b4eae6:/AWS_Lambda/static/Pupil_Output_Images/' + Out_File + ' ~/Output_Data/' + Out_File)
     os.system('scp -i ~/Downloads/imp_benten.pem ubuntu@3.87.214.251:Output_Data/' + Out_File + ' ~/Downloads/' + Out_File)
 
