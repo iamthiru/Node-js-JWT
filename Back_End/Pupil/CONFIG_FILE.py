79fc253f091fa6062fb0369ce32d40b3e424a9cc
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-02-19 13:41:08 -0500

    Update CONFIG_FILE.py

--- Back_End/Pupil/CONFIG_FILE.py
+++ Back_End/Pupil/CONFIG_FILE.py
@@ -6,7 +6,7 @@
 import os
 
 # 1. NIR ; 2. Color
-ch = 1
+# ch = 1
 
 # video filename
 filename = 'Elina_DarkBrown_Vertical_02.MOV'
@@ -14,5 +14,5 @@ filename = 'Elina_DarkBrown_Vertical_02.MOV'
 # video type: NIR or Color
 video_type = 'Color'
 
-command = 'python IMPACT_PUPIL_v1.3.py ' + str(ch) + ' ' + filename + ' ' + video_type
+command = 'python IMPACT_PUPIL_v1.3.py ' + filename + ' ' + video_type
 os.system(command)
