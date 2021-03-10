3dbffc86539d332accd76f80e6806ca1e1da798f
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-03-10 13:40:30 -0500

    Update IMPACT_PUPIL_v1.3.py

--- Back_End/Pupil/IMPACT_PUPIL_v1.3.py
+++ Back_End/Pupil/IMPACT_PUPIL_v1.3.py
@@ -1,7 +1,7 @@
 # Proprietary: Benten Technologies, Inc.
 # Author: Pranav H. Deo
 # Copyright Content
-# Date: 02/19/2021
+# Date: 02/03/2021
 # Version: v1.3
 
 # Code Description:
@@ -10,6 +10,7 @@
 # * Video Quality and Drop Criteria Added (20 Frames).
 
 # UPDATES:
+# * Fixed for 30 FPS
 # * CODE MODULES created
 # * MAJOR UPDATE - Added Dynamic Threshold Detector for Pupil and Iris.
 # * Threshold value decided by looking at first 10 frames.
@@ -27,10 +28,12 @@ import time
 import cv2
 import sys
 import numpy as np
+import pandas as pd
 
 # MODULE IMPORTS
 import Detector
 import Data_Processing
+import PUAL_Gen
 
 # GLOBAL LISTS
 Iris_Dilation = []
@@ -48,12 +51,13 @@ processed_ratio = []
 # INITIALIZATIONS:
 start_time = time.time()
 flag = 0
+token = 'Good'
 count = 0
 counter = 0
 Dropped_Frame_Counter = 0
 frame_array = []
-filename = ''
 size = ()
+
 
 ########################################################################################################################
 
@@ -63,12 +67,11 @@ print("\n************************* IMPACT: PUPIL DETECTION *********************
 if len(sys.argv) > 1:
     filename = str(sys.argv[1])
     video_type = str(sys.argv[2])
-    video = cv2.VideoCapture('/AWS_Lambda/static/Pupil_Input_Videos/' + filename)
-    # video = cv2.VideoCapture('s3://impact-benten/Pupil_Data/Uploads-VideoFiles/' + filename)
+    video = cv2.VideoCapture('./static/Pupil_Input_Videos/' + filename)
 else:
-    video = cv2.VideoCapture('/AWS_Lambda/static/Pupil_Input_Videos/Eric_Test01.mp4')
-    # video = cv2.VideoCapture('s3://impact-benten/Pupil_Data/Uploads-VideoFiles/APKTest03_Elina.mp4')
-
+    filename = 'APKTest03_Elina.mp4'
+    video_type = 'Color'
+    video = cv2.VideoCapture('./static/Pupil_Input_Videos/' + filename)
 
 # Calculate FPS of the Video:
 fps = video.get(cv2.CAP_PROP_FPS)
@@ -81,64 +84,65 @@ Pupil_Thresh, Iris_Thresh = Detector.Threshold_Detect(video)
 
 # Reading Video Frame by Frame:
 while video.isOpened():
-    if counter % 2 == 0:
-        ret, frame = video.read()
+    ret, frame = video.read()
 
-        if ret:
-            frame_num.append(count)
-            count = count + 1
+    if ret:
+        frame_num.append(count)
+        count = count + 1
 
-            if video_type == 'NIR' or video_type == 'Color':
-                file_ext = filename.split(".")[-1]
-                im = frame
-                # im = cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
-                im = cv2.GaussianBlur(im, (5, 5), 0)
-                im = cv2.medianBlur(im, 5)
-                im = cv2.bilateralFilter(im, 9, 75, 75)
-                height, width, layers = im.shape
-                size = (width, height)
-                Pupil, Pupil_center, pupil_radii, pupil_xpoints, pupil_ypoints, Pupil_Dilation = Detector.Pupil_Detection(im, frame_num, Pupil_Thresh, pupil_radii,
-                                                                                                                          pupil_xpoints, pupil_ypoints, Pupil_Dilation, video_type)
-                if len(Pupil_center) == 0 and (len(pupil_xpoints) and len(pupil_ypoints) != 0):
-                    Pupil_center = [pupil_xpoints[-1], pupil_ypoints[-1]]
-                elif len(Pupil_center) == 0:
-                    Dropped_Frame_Counter += 1
-                if Dropped_Frame_Counter > 60:
-                    print('\n# VIDEO DIS-QUALIFIED -> Pupil Undetected.. Retake Video!!!')
+        if video_type == 'NIR' or video_type == 'Color':
+            file_ext = filename.split(".")[-1]
+            im = frame
+            # im = cv2.rotate(im, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            im = cv2.GaussianBlur(im, (5, 5), 0)
+            im = cv2.medianBlur(im, 5)
+            im = cv2.bilateralFilter(im, 9, 75, 75)
+            height, width, layers = im.shape
+            size = (width, height)
+            Pupil, Pupil_center, pupil_radii, pupil_xpoints, pupil_ypoints, Pupil_Dilation = Detector.Pupil_Detection(im, frame_num, Pupil_Thresh, pupil_radii,
+                                                                                                                      pupil_xpoints, pupil_ypoints, Pupil_Dilation, video_type)
+            if len(Pupil_center) == 0 and (len(pupil_xpoints) and len(pupil_ypoints) != 0):
+                Pupil_center = [pupil_xpoints[-1], pupil_ypoints[-1]]
+            elif len(Pupil_center) == 0:
+                Dropped_Frame_Counter += 1
+            if Dropped_Frame_Counter > 40:
+                print('\n# VIDEO DIS-QUALIFIED -> Pupil Undetected.. Retake Video!!!')
+                token = 'Bad'
+                flag = 0
+                break
+            if len(Pupil_center) != 0:
+                if np.average(pupil_radii) >= 35:
+                    Iris, iris_radii, iris_xpoints, iris_ypoints, Iris_Dilation = Detector.Iris_Detection(im, frame_num, Iris_Thresh, Pupil_center,
+                                                                                                          iris_radii, iris_xpoints, iris_ypoints,
+                                                                                                          pupil_radii, Iris_Dilation, video_type)
+                else:
+                    print('\n# VIDEO DIS-QUALIFIED -> Low Pixels for Pupil.. Retake Video!!!')
+                    token = 'Bad'
                     flag = 0
                     break
-                if len(Pupil_center) != 0:
-                    if np.average(pupil_radii) >= 35:
-                        Iris, iris_radii, iris_xpoints, iris_ypoints, Iris_Dilation = Detector.Iris_Detection(im, frame_num, Iris_Thresh, Pupil_center,
-                                                                                                              iris_radii, iris_xpoints, iris_ypoints,
-                                                                                                              pupil_radii, Iris_Dilation, video_type)
-                    else:
-                        print('\n# VIDEO DIS-QUALIFIED -> Low Pixels for Pupil.. Retake Video!!!')
-                        flag = 0
-                        break
 
-                # cv2.imshow('Output', im)
-                frame_array.append(im)
-                flag = 1
+            # cv2.imshow('Output', im)
+            frame_array.append(im)
+            flag = 1
 
-            else:
-                print("WRONG CHOICE !!")
-                break
-
-            key = cv2.waitKey(1)
-            if key == 27 or 0xFF == ord('q'):
-                flag = 1
-                break
         else:
+            print("WRONG CHOICE !!")
+            break
+
+        key = cv2.waitKey(1)
+        if key == 27 or 0xFF == ord('q'):
             flag = 1
             break
+    else:
+        flag = 1
+        break
 
     counter = counter + 1
 
 ########################################################################################################################
 
 # Create Video Output for Visualization
-if flag == 1:
+if flag == 1 and token == 'Good':
     # Creates the video out of the processed frames
     Data_Processing.Output_Visual_Video_Generate(filename, fps, size, frame_array)
     # Process the Pupil and Iris lists : Fill Spaces and Post-Process
@@ -149,6 +153,12 @@ if flag == 1:
     # Graph the values from CSVs into a plot
     Data_Processing.Grapher_Plot_Dilations(filename)
 
+    # Computing the PUAL Score for the data-points collected
+    print('\n***************************** PUAL *****************************')
+    new_DF = pd.read_csv('./static/Pupil_Output_Images/' + filename.split(".")[0] + '_Ratio_Dilation.csv', usecols=['Frame', 'Processed Ratio'])
+    PUAL_Score, Noise, Corrected_PUAL_Score = PUAL_Gen.Calculate_PUAL(filename, np.array(new_DF['Processed Ratio'])[0:256], fps)
+    print('\n> PUAL: ', PUAL_Score, ' ; ', 'Noise: ', Noise, ' ; ', 'Corrected PUAL: ', Corrected_PUAL_Score)
+
 ########################################################################################################################
 
 # Compute Resources being used or utilized
@@ -157,4 +167,5 @@ Data_Processing.Compute_Resources(start_time)
 
 video.release()
 cv2.destroyAllWindows()
+
 ########################################################################################################################
