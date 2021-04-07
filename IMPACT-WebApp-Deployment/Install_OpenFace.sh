89be648c50ae69f4d976d46c6a7ad7125ecb7161
pdeo24 <pdeo@bententech.com>
2021-04-07 12:36:30 -0400

    Updates

--- IMPACT-WebApp-Deployment/Install_OpenFace.sh
+++ IMPACT-WebApp-Deployment/Install_OpenFace.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-apt-get update
-apt-get -y install build-essential
-apt-get -y install g++-8
-apt-get -y install cmake
-apt-get -y install libopenblas-dev
-apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+sudo apt-get update
+sudo apt-get -y install build-essential
+sudo apt-get -y install g++-8
+sudo apt-get -y install cmake
+sudo apt-get -y install libopenblas-dev
+sudo apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 wget https://github.com/opencv/opencv/archive/4.1.0.zip
-apt-get install -y unzip
+sudo apt-get install -y unzip
 unzip 4.1.0.zip
 cd opencv-4.1.0
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_TIFF=ON -D WITH_TBB=ON ..
-make -j2
-make install
+sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_TIFF=ON -D WITH_TBB=ON ..
+sudo make -j2
+sudo make install
 wget http://dlib.net/files/dlib-19.13.tar.bz2
 tar xf dlib-19.13.tar.bz2
 cd dlib-19.13
@@ -22,10 +22,10 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --config Release
-make install
-ldconfig
+sudo make install
+sudo ldconfig
 cd ../..
-apt-get -y install libboost-all-dev
+sudo apt-get -y install libboost-all-dev
 git clone https://github.com/TadasBaltrusaitis/OpenFace.git
 cd OpenFace
 mkdir build
