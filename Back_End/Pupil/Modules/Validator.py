4c0670967272b379b769e855e6420a5d0929236e
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-04-19 15:34:25 -0400

    Update Validator.py

--- Back_End/Pupil/Modules/Validator.py
+++ Back_End/Pupil/Modules/Validator.py
@@ -1,7 +1,7 @@
 # Proprietary: Benten Technologies, Inc.
 # Author: Pranav H. Deo
 # Copyright Content
-# Date: 01/15/2021
+# Date: 04/19/2021
 
 # Module Description:
 # * Radius_Validity_Check - Checks the Current Frame's detected Radius Validity wrt previously recorded Radius values.
@@ -16,8 +16,8 @@ import numpy as np
 def Radius_Validity_Check(radius, input_lst, fr_nm, tag):
     if tag == 'pupil':
         if len(fr_nm) > 0:
-            drop_P = 0.80 * np.mean(input_lst)
-            surge_P = (0.20 + 1) * np.mean(input_lst)
+            drop_P = 0.85 * np.mean(input_lst)
+            surge_P = (0.15 + 1) * np.mean(input_lst)
             if len(fr_nm) > 0 and (radius <= drop_P or radius >= surge_P):
                 R = np.mean(input_lst)
             else:
@@ -26,8 +26,8 @@ def Radius_Validity_Check(radius, input_lst, fr_nm, tag):
             R = radius
     else:
         if len(fr_nm) > 0:
-            drop_I = 0.90 * np.mean(input_lst)
-            surge_I = (1 + 0.10) * np.mean(input_lst)
+            drop_I = 0.95 * np.mean(input_lst)
+            surge_I = (1 + 0.05) * np.mean(input_lst)
             if len(fr_nm) > 0 and (radius <= drop_I or radius >= surge_I):
                 R = np.mean(input_lst)
             else:
