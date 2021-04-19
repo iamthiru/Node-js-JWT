6fe855e9ea75a4bf653a89b1cc6c3485c6cb395b
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-04-19 09:38:04 -0400

    Update Validator.py

--- IMPACT-WebApp-Deployment/Validator.py
+++ IMPACT-WebApp-Deployment/Validator.py
@@ -1,7 +1,7 @@
 # Proprietary: Benten Technologies, Inc.
 # Author: Pranav H. Deo
 # Copyright Content
-# Date: 01/15/2021
+# Date: 04/14/2021
 
 # Module Description:
 # * Radius_Validity_Check - Checks the Current Frame's detected Radius Validity wrt previously recorded Radius values.
@@ -16,8 +16,8 @@ import numpy as np
 def Radius_Validity_Check(radius, input_lst, fr_nm, tag):
     if tag == 'pupil':
         if len(fr_nm) > 0:
-            drop_P = 0.80 * np.mean(input_lst)
-            surge_P = (0.20 + 1) * np.mean(input_lst)
+            drop_P = 0.85 * np.mean(input_lst)
+            surge_P = (0.15 + 1) * np.mean(input_lst)
             if len(fr_nm) > 0 and (radius <= drop_P or radius >= surge_P):
                 R = np.mean(input_lst)
             else:
