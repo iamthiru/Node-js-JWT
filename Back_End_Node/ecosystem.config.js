edd3ffdddb7de7af43e3e20eae99aac7e6c59dec
Monika <mbaranikumar@bententech.com>
2021-05-19 15:13:45 -0400

    config

---
+++ Back_End_Node/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps : [{
+      name: 'TEST_IMPACT_API',
+      script: './bin/www',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8080,
+      },
+      env_testing: {
+        NODE_ENV: 'testing',
+        PORT: 8080,
+      }
+    }],
+  }
