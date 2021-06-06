9fd68cf5485949d1b5735d665c0c6cec1fe61eb3
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-06-06 14:16:21 -0400

    Create impact.sh

---
+++ IMPACT-WebApp-Deployment/impact.sh
@@ -0,0 +1,41 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+#==========================================
+# Installing LAMP
+#==========================================
+apt-get update
+apt-get -y upgrade
+apt install -y apache2
+apt-get install php libapache2-mod-php php-mysql php-curl php-gd php-json php-zip php-mbstring 
+service apache2 restart
+apt-get install -y mysql-server
+#==========================================
+# Installing Docker
+#==========================================
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-get update
+apt install -y docker-ce
+docker login -u hkchang3 -p benten_105
+docker pull hkchang3/impact-benten:v14
+docker run -d -p 5000:5000 --name impact hkchang3/impact-benten:v14
+--//
