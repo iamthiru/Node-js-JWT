486af5d30289943a90028aba88565e15602dc66f
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-06-06 14:19:22 -0400

    Create EC2_Sceduler.py

---
+++ IMPACT-WebApp-Deployment/EC2_Sceduler.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+region = 'us-east-1'
+client = boto3.client('ec2', region_name=region)
+
+
+def lambda_handler(event, context):
+
+    response = client.describe_instances(InstanceIds=['i-0f3b582affba55a00'])
+
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            print(instance["InstanceId"] + "stopping")
+            id = [instance["InstanceId"]]
+            client.stop_instances(InstanceIds=id)
+
+    return("Complete")
