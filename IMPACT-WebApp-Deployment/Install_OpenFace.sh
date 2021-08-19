495e0888fc4a6edf629e71a89314a2c3b4d5548c
Pranav H. Deo <66913206+pdeo24@users.noreply.github.com>
2021-08-19 13:42:40 -0400

    Update Install_OpenFace.sh

--- IMPACT-WebApp-Deployment/Install_OpenFace.sh
+++ IMPACT-WebApp-Deployment/Install_OpenFace.sh
@@ -1,39 +1,59 @@
 # Proprietary: Benten Technologies, Inc.
 # Author: Pranav H. Deo { pdeo@bententech.com }
 # Copyright Content (C)
-# Date: 06/21/2021
+# Date: 08/19/2021
 # Version: v1.0
 #!/bin/bash
-apt-get update
-apt-get -y install build-essential
-apt-get -y install g++-8
-apt-get -y install cmake
-apt-get -y install libopenblas-dev
-apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install gcc-8 g++-8
+sudo apt-get -y install cmake
+sudo apt-get -y install zip
+sudo apt-get -y install libopenblas-dev
+sudo apt-get -y install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev
+sudo apt-get -y install libtbb2 libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+sudo apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+echo "Essential dependencies installed..."
+
+echo "Downloading OpenCV..."
 wget https://github.com/opencv/opencv/archive/4.1.0.zip
 apt-get install -y unzip
 unzip 4.1.0.zip
 cd opencv-4.1.0
-mkdir build
+mkdir -p build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_TIFF=ON -D WITH_TBB=ON ..
-make -j2
-make install
-wget http://dlib.net/files/dlib-19.13.tar.bz2
-tar xf dlib-19.13.tar.bz2
-cd dlib-19.13
-mkdir build
-cd build
-cmake ..
-cmake --build . --config Release
-make install
-ldconfig
+echo "Installing OpenCV..."
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
+make -j4
+sudo make install
 cd ../..
+rm 4.1.0.zip
+sudo rm -r opencv-4.1.0
+echo "OpenCV installed."
+
+echo "Downloading dlib..."
+wget http://dlib.net/files/dlib-19.13.tar.bz2;
+tar xf dlib-19.13.tar.bz2;
+cd dlib-19.13;
+mkdir -p build;
+cd build;
+cmake ..;
+cmake --build . --config Release;
+sudo make install;
+sudo ldconfig;
+cd ../..;
+rm -r dlib-19.13.tar.bz2
+echo "dlib installed"
+
+echo "Installing OpenFace..."
 apt-get -y install libboost-all-dev
 cd OpenFace
-mkdir build
+mkdir -p build
 bash download_models.sh
 cd build
 cmake -D CMAKE_CXX_COMPILER=g++-8 -D CMAKE_C_COMPILER=gcc-8 -D CMAKE_BUILD_TYPE=RELEASE ..
 make
+cd ..
+echo "OpenFace successfully installed."
